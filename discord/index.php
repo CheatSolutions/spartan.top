6c5a87056c2f6c3e6708c559ee4e3705305308b8
Evangelos <vaggdedes@gmail.com>
2026-07-28 19:23:03 +0300

    Update index.php

--- discord/index.php
+++ discord/index.php
@@ -1,3 +1,3 @@
 <?php
 require_once '/var/www/.structure/library/base/utilities.php';
-redirect_to_url('https://idealistic.ai/discord/spartan');
+redirect_to_url('https://discord.com/invite/XCc9XWJDRu');
